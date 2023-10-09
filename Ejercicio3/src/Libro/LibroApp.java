8333e4478b032e0b72c2dba4f5149a87255f628e
OriolMelo <oriolmelosantana@gmail.com>
2023-10-09 17:42:16 +0200

    Main de la clase libro

--- Ejercicio3/src/Libro/LibroApp.java
+++ Ejercicio3/src/Libro/LibroApp.java
@@ -4,8 +4,12 @@ public class LibroApp {
 
 	public static void main(String[] args) {
 
-	//	Libro un_libro = new Libro("123456789", "El primero", )
+		Libro un_libro = new Libro("123456789", "El primero", "Autor", 5);
+		Libro otro_libro = new Libro("987654231", "El otro", "Otro", 10);
 		System.out.println("Hola");
+		
+		un_libro.toString();
+		otro_libro.toString();
 	}
 
 }
