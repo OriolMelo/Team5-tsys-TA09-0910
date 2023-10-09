46e3c13030e0b932232c912e0e1ffc15782c9429
OriolMelo <oriolmelosantana@gmail.com>
2023-10-09 18:34:13 +0200

    Metodo de comparacion de libros

--- Ejercicio3/src/Libro/LibroApp.java
+++ Ejercicio3/src/Libro/LibroApp.java
@@ -5,10 +5,12 @@ public class LibroApp {
 	public static void main(String[] args) {
 
 		Libro un_libro = new Libro("123456789", "El primero", "Autor", 5);
-		Libro otro_libro = new Libro("987654231", "El otro", "Otro", 10);
+		Libro otro_libro = new Libro("987654231", "El otro", "Otro", 1);
 		
 		System.out.println(un_libro.toString());
 		System.out.println(otro_libro.toString());
+		
+		un_libro.comparar(otro_libro);
 	}
 
 }
