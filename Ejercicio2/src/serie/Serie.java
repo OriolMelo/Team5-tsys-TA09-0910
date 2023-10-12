c87d349830ef2ed7634091132158b8946a14c74a
Daniel Nuñez Romero <daninuro96@gmail.com>
2023-10-12 19:37:00 +0200

    Añadimos clase serie

--- Ejercicio2/src/serie/Serie.java
+++ Ejercicio2/src/serie/Serie.java
@@ -1,5 +1,84 @@
 package serie;
 
-public class Serie {
+public class Serie 
+{
+	
+		protected String titulo;
+		protected int num_temporadas;
+		protected boolean entregado;
+		protected String genero;
+		protected String creador;
 
+	
+		public Serie() {
+			this.titulo = "";
+			this.num_temporadas = 3;
+			this.entregado = false;
+			this.genero = "";
+			this.creador = "";
+		}
+		
+		
+		public Serie(String titulo, String creador) {
+			this.titulo = titulo;
+			this.num_temporadas = 3;
+			this.entregado = false;
+			this.genero = "";
+			this.creador = creador;
+		}	
+		
+		
+		public Serie(String titulo, int numTemporadas, String genero, String creador) {
+			this.titulo = titulo;
+			this.num_temporadas = numTemporadas;
+			this.entregado = false;
+			this.genero = genero;
+			this.creador = creador;
+		}
+
+	
+		public String getTitulo() {
+			return titulo;
+		}
+
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+
+		
+
+		public int getNum_temporadas() {
+			return num_temporadas;
+		}
+
+
+		public void setNum_temporadas(int num_temporadas) {
+			this.num_temporadas = num_temporadas;
+		}
+
+
+		public String getGenero() {
+			return genero;
+		}
+
+		public void setGenero(String genero) {
+			this.genero = genero;
+		}
+
+		public String getCreador() {
+			return creador;
+		}
+
+		public void setCreador(String creador) {
+			this.creador = creador;
+		}
+
+		
+		@Override
+		public String toString() {
+			return "Serie [titulo=" + titulo + ", numTemporadas=" + num_temporadas + ", entregado=" + entregado + ", genero="
+					+ genero + ", creador=" + creador + "]";
+		}
+
+	
 }
