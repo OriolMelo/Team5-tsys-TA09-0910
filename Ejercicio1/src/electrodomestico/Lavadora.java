ab43f69ae4d4b064b6102383bbeb18420f5d09fa
OriolMelo <oriolmelosantana@gmail.com>
2023-10-12 19:55:21 +0200

    Arreglado constructor con parámetro carga

--- Ejercicio1/src/electrodomestico/Lavadora.java
+++ Ejercicio1/src/electrodomestico/Lavadora.java
@@ -2,36 +2,36 @@ package electrodomestico;
 
 public class Lavadora extends Electrodomestico {
 
-	private int cargar;
+	private int carga;
 	
 	private final int CARGAR_BASE = 5;
 	
 	public Lavadora() {
-		this.cargar = CARGAR_BASE;
+		this.carga = CARGAR_BASE;
 	}
 
 	public Lavadora(double precio_base, double peso) {
 		super( precio_base, peso);
-		this.cargar = CARGAR_BASE;
+		this.carga = CARGAR_BASE;
 	}
 	
-	public Lavadora(int cargar, double precio_base, String color, char consumo_energetico, double peso) {
+	public Lavadora(int carga, double precio_base, String color, char consumo_energetico, double peso) {
 		super( precio_base, color, consumo_energetico, peso);
-		this.cargar = CARGAR_BASE;
+		this.carga = carga;
 	}
 	
 	public int getCargar() {
-		return cargar;
+		return carga;
 	}
 
-	public void setCargar(int cargar) {
-		this.cargar = cargar;
+	public void setCargar(int carga) {
+		this.carga = carga;
 	}
 	
 	public double precioFinal() {
 		super.precioFinal();
 		
-		if(cargar > 50) {
+		if(carga > 50) {
 			return this.precio_base+=50;
 		}
 		else {
