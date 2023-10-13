a86e9becb50ae3a5683b69eb53148d7f8f1b78fb
OriolMelo <oriolmelosantana@gmail.com>
2023-10-13 21:43:47 +0200

    Añadida clase Asiento

---
+++ Ejercicio6/src/cine/Asiento.java
@@ -0,0 +1,22 @@
+package cine;
+
+public class Asiento {
+	
+	public Asiento(int fila, char columna) {
+		this.fila = fila;
+		this.columna = columna;
+		this.ocupado = OCUPADO_BASE;
+		this.espectador = null;
+	}
+	
+	public void ocupar_asiento(Espectador espectador) {
+		this.espectador= espectador;
+		ocupado=true;
+	}
+	
+	private int fila;
+	private char columna; 
+	private boolean ocupado;
+	private Espectador espectador;
+	private final boolean OCUPADO_BASE = false;
+}
